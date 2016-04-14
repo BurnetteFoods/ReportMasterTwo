a30b57d6458f624d97bf7b4dbc99470de552b611
Anthony Peterson <apeterson@burnettefoods.com>
2016-04-14 12:25:28 -0400

    Fixed AssemblyInfo to restore it to what it was before

--- ReportMasterTwo/Properties/AssemblyInfo.cs
+++ ReportMasterTwo/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("ReportMasterTwo")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Fixed Column and CSV Format Textual Reporting Tool. ReportMasterTwo was originally created to replace a deprecated reporting tool used by Sage MAS 90/200 which allowed precise fixed column layouts of text.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyCompany("Burnette Foods, Inc.")]
 [assembly: AssemblyProduct("ReportMasterTwo")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2016")]
+[assembly: AssemblyCopyright("Copyright © Burnette Foods Inc. 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2.4.0.0")]
+[assembly: AssemblyFileVersion("2.4.0.0")]
