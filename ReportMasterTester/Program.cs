277ec1d6d9a61ae41f580cad9b892b0b6474e6d6
Anthony Peterson <apeterson@burnettefoods.com>
2017-01-17 10:08:08 -0500

    Added tester project

---
+++ ReportMasterTester/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ReportMasterTwo;
+
+namespace ReportMasterTester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ReportMaster rm = new ReportMaster(args[0], args[1], args[2]);
+            rm.Run();
+        }
+    }
+}
